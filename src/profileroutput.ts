69541bec6d0d398aa1e85750dc699b7c523d7031
Daniel Nichols <dando18studios@gmail.com>
2023-07-19 11:25:15 -0400

    update python search path

--- src/profileroutput.ts
+++ src/profileroutput.ts
@@ -200,9 +200,9 @@ export class ProfilerOutput implements vscode.Disposable {
 
     public getTree(): Thenable<ProfilerOutputTree> {
         return new Promise<ProfilerOutputTree>((resolve, reject) => {
-            getPythonPath().then((pythonPath: string | vscode.Uri) => {
+            getPythonPath(["hatchet", "numpy"]).then((pythonPath: string | vscode.Uri) => {
                 console.log(pythonPath);
-                
+
                 const pythonScriptPath = path.join(__dirname, '..', 'src', 'parse_profile.py');
                 this.process = spawn(`${pythonPath}`, [pythonScriptPath, "--profile", this.uri.path, "--type", this.type, "--hot-path"]);
 
@@ -237,7 +237,7 @@ export class ProfilerOutput implements vscode.Disposable {
         
                 // Handle errors in the Python process
                 this.process.on('error', (err: Error) => {
-                reject(err);
+                    reject(err);
                 });
             });
 		});
--- src/util.ts
+++ src/util.ts
@@ -2,6 +2,18 @@ import * as vscode from 'vscode';
 import * as process from 'child_process';
 
 
+function doesPythonHaveModules(pythonPath: string, modules: string[]): boolean {
+    try {
+        let output = process.execSync(`${pythonPath} -c \"import ${modules.join("; import ")}\"`);
+        if (output) {
+            return true;
+        }
+    } catch (e) {
+        return false;
+    }
+    return false;
+}
+
 /** A helper function to find the correct python to use in the shell.
  * First checks if the Python extension is installed, and if so, uses the
  * python.interpreterPath. 
@@ -10,29 +22,34 @@ import * as process from 'child_process';
  * Otherwise, return "python"
  * @returns the path to the python executable
  */
-export async function getPythonPath(): Promise<string | vscode.Uri> {
+export async function getPythonPath(imports?: string[]): Promise<string | vscode.Uri> {
 
     // check if the Python extension is installed
     const pythonExtension = vscode.extensions.getExtension("ms-python.python");
     if (pythonExtension) {
         const python = await pythonExtension.activate();
         if (python && python.interpreterPath) {
-            return python.interpreterPath;
+            if (!imports || doesPythonHaveModules(python.interpreterPath, imports)) {
+                return python.interpreterPath;
+            }
         }
     }
 
     // check if the python.pythonPath setting is set
     const pythonPath = vscode.workspace.getConfiguration("python").get("pythonPath");
     if (pythonPath) {
-        console.log(pythonPath);
-        return pythonPath.toString();
+        if (!imports || doesPythonHaveModules(pythonPath.toString(), imports)) {
+            return pythonPath.toString();
+        }
     }
 
     // check if python3 is available
     try {
         let output = process.execSync("python3 --version");
         if (output) {
-            return "python3";
+            if (!imports || doesPythonHaveModules("python3", imports)) {
+                return "python3";
+            }
         }
     } catch (e) {}
 
